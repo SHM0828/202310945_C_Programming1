b1818eb6cd58c8ff06c103ff87aa3c06b85f6173
SHM0828 <shm0828.g@gmail.com>
2023-09-15 18:01:15 +0900

    hw1

--- hw1.c
+++ hw1.c
@@ -4,9 +4,9 @@
 int main(void)
 {
 	int var1,var2;
-	printf("Input two integers: ");scanf("%d %d", &var1, &var2);	/// 정수 두개 입력받기
-	printf("%d & %d = %d\n", var1, var2, var1 & var2);				/// AND
-	printf("%d | %d = %d\n", var1, var2, var1 | var2);				/// OR
-	printf("%d ^ %d = %d\n", var1, var2, var1 ^ var2);				/// XOR
+	printf("Input two integers: ");scanf("%d %d", &var1, &var2);
+	printf("%d & %d = %d\n", var1, var2, var1 & var2);	/// AND
+	printf("%d | %d = %d\n", var1, var2, var1 | var2);	/// OR
+	printf("%d ^ %d = %d\n", var1, var2, var1 ^ var2);	/// XOR
 	return 0;
 }
