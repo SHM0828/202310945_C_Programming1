a32e8a7b26f37985dce7cd70625041f0aa061321
SHM0828 <shm0828.g@gmail.com>
2023-09-15 18:29:01 +0900

    hw1

--- hw1.c
+++ hw1.c
@@ -5,8 +5,8 @@ int main(void)
 {
 	int var1,var2;
 	printf("Input two integers: ");scanf("%d %d", &var1, &var2);
-	printf("%d & %d = %d\n", var1, var2, var1 & var2);	/// AND
-	printf("%d | %d = %d\n", var1, var2, var1 | var2);	/// OR
-	printf("%d ^ %d = %d\n", var1, var2, var1 ^ var2);	/// XOR
+	printf("%d & %d = %d\n", var1, var2, var1 & var2);	// AND
+	printf("%d | %d = %d\n", var1, var2, var1 | var2);	// OR
+	printf("%d ^ %d = %d\n", var1, var2, var1 ^ var2);	// XOR
 	return 0;
 }
